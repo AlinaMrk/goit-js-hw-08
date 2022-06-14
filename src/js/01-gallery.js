7b1a96f9f9252fa33c70a13305cc634af27e9336
AlinaMrk <alinamarunchak@yahoo.com>
2022-06-14 16:16:33 +0300

    goit-js-hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,51 +1,26 @@
-
-/*import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items';
-console.log(galleryItems);
-const galleryList = document.querySelector('.gallery')
-
-    const addGallery = (imgArray) => {
-    return imgArray.map(({ preview, description, original }) => {
-        return `
-        <div class="gallery__item">
-            <a class="gallery__link" href=${original}>
-                <img src=${preview} title=${description}  alt=${description}  data-source=${original} class="gallery__image">
-            </a>
-        </div>`;
-    }).join('')
-    }
-
-    let cardSet = addGallery(galleryItems)
-    galleryList.insertAdjacentHTML('afterbegin',cardSet )
-    
-    let lightbox = new SimpleLightbox('.gallery a', {
-    
-    overlay: true,
-    captionDelay: '250ms'
-});
-*/
-import { galleryItems } from './gallery-items.js';
-
 import SimpleLightbox from 'simplelightbox';
-
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
 
 const galleryRef = document.querySelector('.gallery');
+const itemMakrup = createGalleryMarkup(galleryItems);
 
-function createGallery(items) {
-  items.map(item => {
-    const markup = `<a class="gallery__item" href="${item.original}">
-    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
-  </a>`;
+galleryRef.insertAdjacentHTML('beforeend', itemMakrup);
 
-    galleryRef.insertAdjacentHTML('beforeend', markup);
-  });
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>`
+    })
+        .join('');
 }
 
-createGallery(galleryItems);
+let lightbox = new SimpleLightbox('.gallery__item', {captionsData: 'alt', captionDelay: 250, });
+lightbox.on('show.simplelightbox', function () { });
 
-const lightbox = new SimpleLightbox('.gallery__item', {
-  captionDelay: 250,
-  captionsData: 'alt',
-});
+
